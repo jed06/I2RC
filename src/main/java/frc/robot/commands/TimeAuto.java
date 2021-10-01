679a06c85203c297563a5c60c88bdbd7b7ff0dc2
jed06 <jessicadavid060@gmail.com>
2021-10-01 16:26:21 -0700

    Created autonomous timer

--- src/main/java/frc/robot/commands/TimeAuto.java
+++ src/main/java/frc/robot/commands/TimeAuto.java
@@ -1,39 +1,49 @@
-W// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
 
-public class TankDrive extends CommandBase {
+public class TimeAuto extends CommandBase {
+private Timer timer;
+private DriveTrain driveTrain;
 
-  private final DriveTrain _driveTrain;
-  private final Joystick _leftJoystick;
-  private final Joystick _rightJoystick;
-  
-  /** Creates a new TankDrive. */
-  public TankDrive(DriveTrain dt, Joystick lj, Joystick rj) {
+
+  public TimeAuto(DriveTrain dt) {
     // Use addRequirements() here to declare subsystem dependencies.
-    _driveTrain = dt;
-    _leftJoystick = lj;
-    _rightJoystick = rj;
-
-    addRequirements(_driveTrain);
+    timer  = new Timer();
+    driveTrain = dt;
+    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    timer.start();
+    timer.reset();
+
+
+
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    _driveTrain.tankDrive(-0.8 * _leftJoystick.getRawAxis(Constants.JoystickAxis.YAxis),
-                          -0.8 * _rightJoystick.getRawAxis(Constants.JoystickAxis.YAxis));
+    if ( timer.get() <= 5){
+      driveTrain.tankDrive(1, 0.5);
+    }
+    if ( timer. get() > 5){
+      driveTrain.tankDrive(0, 0);
+    }
+
+
+
+
   }
 
   // Called once the command ends or is interrupted.
